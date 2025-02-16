8a99b6062746f514a47909d85f92bdac8670fec7
ccttn <102339663+ccttn@users.noreply.github.com>
2025-02-16 17:22:52 +0100

    Update app.py

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-landing_page = st.Page("landingpage.py", title="HeyDoc Chatbot", icon=":material/home:")
+landing_page = st.Page("landingpage2.py", title="HeyDoc Chatbot", icon=":material/home:")
 perso_page = st.Page("personnal_info.py", title="Personal Info", icon=":material/settings_accessibility:")
 
 pg = st.navigation([landing_page, perso_page])
