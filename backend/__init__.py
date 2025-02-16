1218b4016f55ee4c5b024dffcc301dd80c257472
Jean Sebastian French <sebas03446@gmail.com>
2025-02-16 13:41:27 +0100

    feat

---
+++ backend/__init__.py
@@ -0,0 +1,3 @@
+from backend.app.services.medical_api import get_symptoms, get_specialisations
+
+__all__ = ["get_symptoms", "get_specialisations"]
---
+++ backend/app/__init__.py
@@ -0,0 +1,4 @@
+from .services import *
+from .routes import *
+
+__version__ = "0.1.0"
